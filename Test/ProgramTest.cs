b76c90b16a250dac59de7cbe317c49ff2254e5e1
Dutchpainter <Yolanda.ter.horst@xs4all.nl>
2020-12-29 13:41:40 +0100

    sdsd

--- Test/ProgramTest.cs
+++ Test/ProgramTest.cs
@@ -2,125 +2,71 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
-namespace Op4
+namespace Dag_01
 {
-	class Program
-	{
-		async static Task Main(string[] args)
-		{
-			var regex = new Regex("(?'key'[a-zA-Z]{1,3}):(?'value'[a-zA-Z0-9#]+)", RegexOptions.Compiled);
-			var hclRgex = new Regex("#[0-9a-f]{6}", RegexOptions.Compiled);
-			var eclOptions = new[] { "amb", "blu", "brn", "gry", "grn", "hzl", "oth" };
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var file = new FileStream("Input.txt", FileMode.Open);
+            using var reader = new StreamReader(file);
+            // lijst voor alle getallen
+            var getallen = new List<int>();
 
-			var keyDescr = new Dictionary<string, (string key, bool required, Func<string, bool> validator)>
-			{
-				["byr"] = ("byr", true, s => s.Length == 4 && int.TryParse(s, out var byr) && byr >= 1920 && byr <= 2002),
-				["iyr"] = ("iyr", true, s => s.Length == 4 && int.TryParse(s, out var iyr) && iyr >= 2010 && iyr <= 2020),
-				["eyr"] = ("eyr", true, s => s.Length == 4 && int.TryParse(s, out var iyr) && iyr >= 2020 && iyr <= 2030),
-				["hgt"] = ("hgt", true, s =>
-				{
-					if (s.EndsWith("cm"))
-					{
-						var hgtStr = s.Substring(0, s.Length - 2);
-						return int.TryParse(hgtStr, out var hgt) && hgt >= 150 && hgt <= 193;
-					}
-					if (s.EndsWith("in"))
-					{
-						var hgtStr = s.Substring(0, s.Length - 2);
-						return int.TryParse(hgtStr, out var hgt) && hgt >= 59 && hgt <= 76;
-					}
+            // inlezen getallen
+            while (true)
+            {
+                var regel = reader.ReadLine();
+                if (string.IsNullOrEmpty(regel))
+                    break;
 
-					return false;
-				}
-				),
-				["hcl"] = ("hcl", true, s => hclRgex.IsMatch(s)),
-				["ecl"] = ("ecl", true, s => eclOptions.Any(t => t == s)),
-				["pid"] = ("pid", true, s => s.Length == 9 && int.TryParse(s, out var _)),
-				["cid"] = ("cid", false, s => true)
-			};
-			int reqCount = keyDescr.Count(k => k.Value.required);
+                getallen.Add(int.Parse(regel));
 
-			using var file = new FileStream("input.txt", FileMode.Open);
-			using var reader = new StreamReader(file);
+            }
+            // deel 1 van de vraag oplossen
+            // controleren of er een eersteGetal en een tweedeGetal zijn die samen 2020 opleveren
+            foreach (var eersteGetal in getallen)
+            {
+                var benodigdGetal = 2020 - eersteGetal;
+                var tweedeGetal = getallen
+                    .Where(getal => getal == benodigdGetal)
+                    .ToList();
 
-			var validPassports = 0;
-			var validPassportsWithValidation = 0;
-			while (true)
-			{
-				string lastLine;
-				var lines = new List<string>();
-				while (true)
-				{
-					lastLine = await reader.ReadLineAsync();
-					if (string.IsNullOrEmpty(lastLine))
-						break;
-					lines.Add(lastLine);
-				}
+                // als er een resultaat is de oplossing printen
+                if (tweedeGetal.Any())
+                {
+                    Console.WriteLine("Oplossing deel 1:");
+                    Console.WriteLine($"Eerste getal: {eersteGetal} Tweede getal: {tweedeGetal.Single()} Som: {eersteGetal + tweedeGetal.Single()} Product: {eersteGetal * tweedeGetal.Single()}");
+                    Console.WriteLine();
 
-				if (lastLine == null) break;
+                    // als er een waarde gevonden is:Break. Er worden minimaal 2 sets gevonden
+                    break;
+                }
+            }
+            // deel 2 van de vraag oplossen
+            // controleren of er een eersteGetal, een tweedeGetal en een derdeGetal zijn die samen 2020 opleveren
 
-				var elems = new List<(string key, string value)>();
-				foreach (var line in lines)
-				{
-					var match = regex.Match(line);
-					while (match.Success)
-					{
-						elems.Add((match.Groups["key"].Value, match.Groups["value"].Value));
-						match = match.NextMatch();
-					}
-				}
+            Console.WriteLine("Oplossing deel 2:");
+            foreach (var eersteGetal in getallen)
+            {
+                var benodigdGetalEen = 2020 - eersteGetal;
+                foreach (var tweedeGetal in getallen.Where(getal => (getal != eersteGetal)).ToList())
+                {
+                    var benodigdGetalTwee = benodigdGetalEen - tweedeGetal;
+                    var derdeGetal = getallen
+                        .Where(getal => (getal == benodigdGetalTwee && getal != eersteGetal && getal != tweedeGetal))
+                        .ToList();
 
-				var foundCount = new Dictionary<string, int>();
-				var spares = new List<string>();
-				foreach (var elem in elems)
-				{
-					if (keyDescr.ContainsKey(elem.key))
-					{
-						if (foundCount.ContainsKey(elem.key))
-						{
-							foundCount[elem.key]++;
-						}
-						else
-						{
-							foundCount[elem.key] = 1;
-						}
+                    if (derdeGetal.Any())
+                    {
+                        Console.WriteLine($"Eerste getal: {eersteGetal} Tweede getal: {tweedeGetal} Derde getal: {derdeGetal.Single()} Som: {eersteGetal + tweedeGetal + derdeGetal.Single()} Product: {eersteGetal * tweedeGetal * derdeGetal.Single()}");
+                        break;
+                    }
 
-					}
-					else
-					{
-						spares.Add(elem.key);
-					}
-				}
-
-				if (spares.Count > 0) continue;
-				if (foundCount.Values.Any(t => t > 1)) continue;
-
-				var foundRequiredKeys = 0;
-				foreach (var key in foundCount.Keys)
-				{
-					if (keyDescr.TryGetValue(key, out var descr))
-					{
-						if (descr.required) foundRequiredKeys++;
-					}
-				}
-
-				if (foundRequiredKeys == reqCount)
-				{
-					validPassports++;
-					if (foundCount
-						.All(kvp =>
-							keyDescr[kvp.Key].validator(elems.Single(el => el.key == kvp.Key).value)))
-						validPassportsWithValidation++;
-				}
-
-
-			}
-
-			Console.WriteLine(validPassports);
-			Console.WriteLine(validPassportsWithValidation);
-		}
-	}
+                }
+            }
+            Console.WriteLine("Opdracht afgerond");
+        }
+    }
 }
